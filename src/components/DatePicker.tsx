9e668843b8c6d158c609a4b49094cbd07a2ab6bf
ViGF <vitoriogdef@gmail.com>
2023-07-07 13:10:02 -0300

    fix: date picker date format

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -29,6 +29,7 @@ function DatePickerComponent(
     <div className="flex w-full flex-col">
       <DatePicker
         locale="pt-BR"
+        dateFormat="dd/MM/yyyy"
         wrapperClassName="w-full"
         className={inputClassName}
         enableTabLoop={false}
