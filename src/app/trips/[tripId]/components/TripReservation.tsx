429499495e4e957b86caf72451dd2a3e86271bf0
ViGF <vitoriogdef@gmail.com>
2023-07-07 09:55:26 -0300

    style: fix padding of trip reservation component

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -27,7 +27,7 @@ export function TripReservation({ trip }: TripReservationProps) {
         <p className="font-medium text-sm text-primaryDarker">Total (7 noites)</p>
         <p className="font-medium text-sm text-primaryDarker">R$2500</p>
       </div>
-      <div className="pb-10 border-b border-b-border-grayLighter w-full">
+      <div className="pb-[2.5rem] mb-[2.5rem] border-b border-b-grayLighter w-full">
         <Button className="mt-3 w-full">Reservar agora</Button>
       </div>
     </div>
