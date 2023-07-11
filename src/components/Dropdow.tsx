4eccf57fed4a3adf3772e2197d39e1fdd3df1879
ViGF <vitoriogdef@gmail.com>
2023-07-11 09:09:14 -0300

    feat: add my trips link to header

--- src/components/Dropdow.tsx
+++ src/components/Dropdow.tsx
@@ -1,5 +1,6 @@
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { signOut } from 'next-auth/react'
+import Link from 'next/link';
 import { HiBars3 } from 'react-icons/hi2'
 
 export function Dropdown() {
@@ -15,8 +16,14 @@ export function Dropdown() {
         </button>
       </DropdownMenu.Trigger>
       <DropdownMenu.Portal>
-        <DropdownMenu.Content className='p-2 bg-white rounded-md shadow-md'>
-          <DropdownMenu.Item>
+        <DropdownMenu.Content className='p-2 pl-3 bg-white rounded-md shadow-md'>
+          <DropdownMenu.Item className='py-1'>
+            <Link href='/my-trips' className='text-primary text-sm font-semibold hover:border-0 hover:text-primaryDarker'>
+              Minhas viagens
+            </Link>
+          </DropdownMenu.Item>
+          <DropdownMenu.Separator className='border-t border-primary' />
+          <DropdownMenu.Item className='py-1'>
             <button className='text-primary text-sm font-semibold hover:border-0 hover:text-primaryDarker' onClick={handleLogoutClick}>
               Logout
             </button>
