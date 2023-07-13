156114a5f7d2d79d850067c6046a8f7e37bb9ce2
ViGF <vitoriogdef@gmail.com>
2023-07-13 17:10:31 -0300

    style: adapt search page to desktop

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -29,15 +29,15 @@ export default async function Trip({ searchParams }: TripProps) {
 
   return (
     <div className="container mx-auto flex-col items-center p-5">
-      <h1 className="text-xl font-semibold text-primaryDarker">
+      <h1 className="text-xl font-semibold text-primaryDarker lg:text-3xl">
         Hospedagens encontradas
       </h1>
-      <p className="font-medium text-dark">
+      <p className="font-medium text-dark lg:mt-6">
         {trips.length > 0
           ? "Listamos as melhores hospedagens para você"
           : "Não encontramos nenhuma acomodação"}
       </p>
-      <div className="mt-2 flex flex-col gap-4">
+      <div className="mt-2 flex flex-col gap-4 lg:mt-6 lg:grid lg:grid-cols-4 lg:gap-10 lg:pb-6">
         {trips.map((trip) => (
           <TripItem key={trip.id} trip={trip} />
         ))}
