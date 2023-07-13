79b2cf6657f9e3dd49b432a5c704b179be9efe30
ViGF <vitoriogdef@gmail.com>
2023-07-13 13:59:48 -0300

    feat: add stripe payment

---
+++ src/app/api/payment/route.ts
@@ -0,0 +1,45 @@
+import { getServerSession } from "next-auth";
+import {  } from "next-auth/react";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+import { authOptions } from "../auth/[...nextauth]/route";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: '2022-11-15'
+})
+
+export async function POST(request: Request) {
+  const req = await request.json()
+  const userSession = await getServerSession(authOptions)
+
+  const { totalPrice, name, description, coverImage, startDate, endDate, guests, tripId } = req
+
+  const session = await stripe.checkout.sessions.create({
+    success_url: 'http://localhost:3000/my-trips',
+    metadata: {
+      startDate,
+      endDate,
+      guests,
+      tripId,
+      totalPrice,
+      userId: (userSession?.user as any).id
+    },
+    line_items: [
+      {
+        price_data: {
+          currency: 'brl',
+          unit_amount: totalPrice * 100,
+          product_data: {
+            name,
+            description,
+            images: [coverImage]
+          }
+        },
+        quantity: 1
+      }
+    ],
+    mode: 'payment'
+  })
+
+  return NextResponse.json({ sessionId: session.id }, { status: 200 })
+}
--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -1,141 +1,157 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from 'react'
-import { useSession } from "next-auth/react"
-import Image from "next/image"
-import { useRouter } from "next/navigation"
-import { Trip } from "@prisma/client"
-import ReactCountryFlag from "react-country-flag"
-import { format } from "date-fns"
-import ptBR from "date-fns/locale/pt-BR"
-import { Button } from "@/components/Button"
-import { toast } from 'react-toastify'
-
-
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Trip } from "@prisma/client";
+import ReactCountryFlag from "react-country-flag";
+import { format } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { Button } from "@/components/Button";
+import { toast } from "react-toastify";
+import { loadStripe } from "@stripe/stripe-js";
 interface TripConfirmationProps {
   params: {
-    tripId: string
-  }
+    tripId: string;
+  };
   searchParams: {
-    startDate: string
-    endDate: string
-    guests: number
-  }
+    startDate: string;
+    endDate: string;
+    guests: number;
+  };
 }
 
-export default function TripConfirmation({ params, searchParams }: TripConfirmationProps) {
-  const [isLoading, setIsLoading] = useState(true)
-  const [trip, setTrip] = useState<Trip | null>(null)
-  const [totalPrice, setTotalPrice] = useState(0)
+export default function TripConfirmation({
+  params,
+  searchParams,
+}: TripConfirmationProps) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [trip, setTrip] = useState<Trip | null>(null);
+  const [totalPrice, setTotalPrice] = useState(0);
 
-  const router = useRouter()
-  const { status, data } = useSession()
+  const router = useRouter();
+  const { status, data } = useSession();
 
   async function handleBuyClick() {
-    const response = await fetch('http://localhost:3000/api/trips/reservation', {
-      method: 'POST',
+    const response = await fetch("http://localhost:3000/api/payment", {
+      method: "POST",
       body: JSON.stringify({
         tripId: params.tripId,
         startDate: searchParams.startDate,
         endDate: searchParams.endDate,
         guests: +searchParams.guests,
-        userId: (data?.user as any).id,
-        totalPaid: totalPrice
-      })
-    }).then(res => res.json())
+        totalPrice,
+        coverImage: trip?.coverImage,
+        name: trip?.name,
+        description: trip?.description
+      }),
+    }).then((res) => res.json());
 
-    if (!response?.sucess) {
-      return toast.error("Não foi possível realizar a reserva", {
-        position: 'bottom-right'
-      })
+    if (!response?.sessionId) {
+      return toast.error("Não foi possível finalizar a compra", {
+        position: "bottom-right",
+      });
     }
 
-    router.push('/')
+    const stripe = await loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_KEY as string
+    );
+
+    await stripe?.redirectToCheckout({
+      sessionId: response.sessionId,
+    });
 
     toast.success("Reserva realizada com sucesso", {
-      position: 'bottom-right'
-    })
+      position: "bottom-right",
+    });
   }
 
   useEffect(() => {
     const fetchTrip = async () => {
-      const response = await fetch('/api/trips/check', {
-        method: 'POST',
+      const response = await fetch("/api/trips/check", {
+        method: "POST",
         body: JSON.stringify({
           tripId: params.tripId,
           startDate: new Date(searchParams.startDate),
-          endDate: new Date(searchParams.endDate)
-        })
-      }).then(res => res.json())
+          endDate: new Date(searchParams.endDate),
+        }),
+      }).then((res) => res.json());
 
       if (response?.error) {
-        return router.push('/')
+        return router.push("/");
       }
 
-      setTrip(response.trip)
-      setTotalPrice(response.totalPrice)
-      setIsLoading(false)
+      setTrip(response.trip);
+      setTotalPrice(response.totalPrice);
+      setIsLoading(false);
+    };
+
+    if (status === "unauthenticated") {
+      return router.push("/");
     }
 
-    if (status === 'unauthenticated') {
-      return router.push('/')
+    if (status === "authenticated") {
+      fetchTrip();
     }
-
-    if (status === 'authenticated') {
-      fetchTrip()
-    }
-  }, [status])
+  }, [status]);
 
   if (isLoading) {
-    return <p>Carregando informações...</p>
+    return <p>Carregando informações...</p>;
   }
 
   if (!trip) {
-    return <p>Acomodação não encontrada</p>
+    return <p>Acomodação não encontrada</p>;
   }
 
-  const startDate = new Date(searchParams.startDate)
-  const endDate = new Date(searchParams.endDate)
+  const startDate = new Date(searchParams.startDate);
+  const endDate = new Date(searchParams.endDate);
 
   return (
     <main className="container mx-auto p-5">
-      <h1 className="font-semibold text-xl text-primaryDarker">Sua viagem</h1>
+      <h1 className="text-xl font-semibold text-primaryDarker">Sua viagem</h1>
       {/* CARD */}
-      <div className="flex flex-col p-5 mt-5 border shadow-lg rounded-lg gap-3">
-        <div className="flex items-center gap-3 pb-5 border-b">
+      <div className="mt-5 flex flex-col gap-3 rounded-lg border p-5 shadow-lg">
+        <div className="flex items-center gap-3 border-b pb-5">
           <Image
             src={trip.coverImage}
             width={106}
             quality={100}
             height={124}
             alt={trip.name}
-            className="object-fill w-full h-auto max-h-[400px] max-w-[556px] rounded-lg"
+            className="h-auto max-h-[400px] w-full max-w-[556px] rounded-lg object-fill"
           />
-          <div className="flex flex-col w-full">
-            <h2 className="text-xl text-primaryDarker font-semibold">{trip.name}</h2>
+          <div className="flex w-full flex-col">
+            <h2 className="text-xl font-semibold text-primaryDarker">
+              {trip.name}
+            </h2>
             <div className="flex items-center gap-1">
               <ReactCountryFlag countryCode={trip.countryCode} svg />
               <p className="text-xs text-dark underline">{trip.location}</p>
             </div>
           </div>
         </div>
-        <h3 className="font-semibold text-md text-primaryDarker mt-1">Informações sobre o preço</h3>
-        <div className="flex justify-between mt-1">
+        <h3 className="text-md mt-1 font-semibold text-primaryDarker">
+          Informações sobre o preço
+        </h3>
+        <div className="mt-1 flex justify-between">
           <p className="text-sm text-primaryDarker">Total</p>
-          <p className="font-semibold text-sm text-primaryDarker">R${totalPrice}</p>
+          <p className="text-sm font-semibold text-primaryDarker">
+            R${totalPrice}
+          </p>
         </div>
       </div>
       {/* INFOS */}
-      <div className="flex flex-col mt-5">
+      <div className="mt-5 flex flex-col">
         <h3 className="font-semibold text-primaryDarker">Data</h3>
-        <div className="flex items-center gap-1 mt-1">
+        <div className="mt-1 flex items-center gap-1">
           <p>
             {format(startDate, "dd", {
-              locale: ptBR
+              locale: ptBR,
             })}
-            {' - '}
+            {" - "}
             {format(endDate, "dd 'de' MMMM", {
-              locale: ptBR
+              locale: ptBR,
             })}
           </p>
         </div>
@@ -143,8 +159,10 @@ export default function TripConfirmation({ params, searchParams }: TripConfirmat
         <h3 className="font-semibold text-primaryDarker">Hóspedes</h3>
         <p>{searchParams.guests} hóspede(s)</p>
 
-        <Button className="mt-5" onClick={handleBuyClick}>Finalizar Compra</Button>
+        <Button className="mt-5" onClick={handleBuyClick}>
+          Finalizar Compra
+        </Button>
       </div>
     </main>
-  )
+  );
 }
