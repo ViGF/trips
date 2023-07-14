315aff499656b0d7a5082a8cd7b00732a60af629
ViGF <vitoriogdef@gmail.com>
2023-07-13 21:29:51 -0300

    style: adapt my trips page to desktop

--- src/app/my-trips/components/UserReservationItem.tsx
+++ src/app/my-trips/components/UserReservationItem.tsx
@@ -8,79 +8,93 @@ import { toast } from "react-toastify";
 
 interface UserReservationItemProps {
   reservation: Prisma.TripReservationGetPayload<{
-    include: { trip: true }
-  }>
-  fetchReservations: () => void
+    include: { trip: true };
+  }>;
+  fetchReservations: () => void;
 }
 
-export function UserReservationItem({ reservation, fetchReservations }: UserReservationItemProps) {
+export function UserReservationItem({
+  reservation,
+  fetchReservations,
+}: UserReservationItemProps) {
   async function handleDeleteClick() {
     const response = await fetch(`/api/trips/reservation/${reservation.id}`, {
-      method: 'DELETE'
+      method: "DELETE",
     })
-      .then(res => res?.json())
+      .then((res) => res?.json())
       .catch(() => {
         return toast.error("Não foi possível cancelar a reserva", {
-          position: 'bottom-right'
-        })
-      })
+          position: "bottom-right",
+        });
+      });
 
     if (response.id) {
       toast.success("A reserva foi cancelada com sucesso", {
-        position: 'bottom-right'
-      })
+        position: "bottom-right",
+      });
 
-      fetchReservations()
+      fetchReservations();
     }
   }
 
   return (
-    <div className="flex flex-col p-5 mt-5 border shadow-lg rounded-lg gap-3">
-      <div className="flex items-center gap-3 pb-5 border-b">
+    <div className="mt-5 flex flex-col gap-3 rounded-lg border p-5 shadow-lg">
+      <div className="flex items-center gap-3 border-b pb-5">
         <Image
           src={reservation.trip.coverImage}
           width={106}
           quality={100}
           height={124}
           alt={reservation.trip.name}
-          className="object-fill w-full h-auto max-h-[400px] max-w-[556px] rounded-lg"
+          className="h-auto max-h-[400px] w-full max-w-[556px] rounded-lg object-fill"
         />
-        <div className="flex flex-col w-full">
-          <h2 className="text-xl text-primaryDarker">{reservation.trip.name}</h2>
+        <div className="flex w-full flex-col">
+          <h2 className="text-xl text-primaryDarker">
+            {reservation.trip.name}
+          </h2>
           <div className="flex items-center gap-1">
             <ReactCountryFlag countryCode={reservation.trip.countryCode} svg />
-            <p className="text-xs text-dark underline">{reservation.trip.location}</p>
+            <p className="text-xs text-dark underline">
+              {reservation.trip.location}
+            </p>
           </div>
         </div>
       </div>
-      <div className="flex items-center justify-between pb-1">
+      <div className="flex lg:flex-col items-center lg:items-start justify-between pb-1">
         <div>
-          <h3 className="text-primaryDarker text-sm">Data</h3>
-          <div className="flex items-center gap-1 mt-1">
+          <h2 className="text-md font-semibold text-primaryDarker py-1">Sobre a viagem</h2>
+          <h3 className="text-sm text-primaryDarker">Data</h3>
+          <div className="mt-1 flex items-center gap-1">
             <p className="text-sm">
               {format(new Date(reservation.startDate), "dd", {
-                locale: ptBR
+                locale: ptBR,
               })}
-              {' - '}
+              {" - "}
               {format(new Date(reservation.endDate), "dd 'de' MMMM", {
-                locale: ptBR
+                locale: ptBR,
               })}
             </p>
           </div>
         </div>
         <div>
-          <h3 className="text-primaryDarker text-sm">Hóspedes</h3>
+          <h3 className="text-sm text-primaryDarker lg:mt-4">Hóspedes</h3>
           <p className="text-sm">{reservation.guests} hóspede(s)</p>
         </div>
       </div>
-      <div className="pt-3 border-t mb-5">
-        <h3 className="text-sm text-primaryDarker mt-1">Informações sobre o preço</h3>
-        <div className="flex justify-between mt-1">
+      <div className="mb-5 border-t pt-3">
+        <h3 className="mt-1 text-sm font-semibold text-primaryDarker">
+          Informações sobre o preço
+        </h3>
+        <div className="mt-1 flex justify-between">
           <p className="text-sm text-primaryDarker">Total</p>
-          <p className="text-sm font-medium text-primaryDarker">R${+reservation.totalPaid}</p>
+          <p className="text-sm font-medium text-primaryDarker">
+            R${+reservation.totalPaid}
+          </p>
         </div>
       </div>
-      <Button variant="danger" onClick={handleDeleteClick}>Cancelar</Button>
+      <Button variant="danger" onClick={handleDeleteClick}>
+        Cancelar
+      </Button>
     </div>
-  )
+  );
 }
--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -1,54 +1,68 @@
-'use client'
+"use client";
 
-import { useRouter } from "next/navigation"
-import { useSession } from "next-auth/react"
-import { useEffect, useState } from "react"
-import { Prisma } from "@prisma/client"
-import { UserReservationItem } from "./components/UserReservationItem"
-import { Button } from "@/components/Button"
-import Link from "next/link"
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { Prisma } from "@prisma/client";
+import { UserReservationItem } from "./components/UserReservationItem";
+import { Button } from "@/components/Button";
+import Link from "next/link";
 
 export default function MyTrips() {
-  const [reservations, setReservations] = useState<Prisma.TripReservationGetPayload<{
-    include: { trip: true }
-  }>[]>([])
+  const [reservations, setReservations] = useState<
+    Prisma.TripReservationGetPayload<{
+      include: { trip: true };
+    }>[]
+  >([]);
 
-  const router = useRouter()
-  const { status, data } = useSession()
+  const router = useRouter();
+  const { status, data } = useSession();
 
   const fetchReservations = async () => {
     const response = await fetch(
       `/api/trips/reservation/user/${(data?.user as any).id}`
-    ).then(res => res.json())
+    ).then((res) => res.json());
 
-    setReservations(response)
-  }
+    setReservations(response);
+  };
 
   useEffect(() => {
-    if (status === 'unauthenticated') {
-      return router.push('/api/auth/signin')
+    if (status === "unauthenticated") {
+      return router.push("/api/auth/signin");
     }
 
-    if (status === 'loading') {
-      return
+    if (status === "loading") {
+      return;
     }
 
-    fetchReservations()
-  }, [status])
+    fetchReservations();
+  }, [status]);
 
   return (
-    <main className="container flex h-full flex-col justify-center p-5">
-      <h1 className="font-semibold text-primaryDarker text-xl">Minhas viagens</h1>
-      {reservations.length > 0 ? (reservations.map(reservation => (
-        <UserReservationItem key={reservation.id} reservation={reservation} fetchReservations={fetchReservations} />
-      ))) : (
-        <div className="flex flex-1 flex-col justify-center">
-          <p className="font-medium text-primaryDarker text-center mt-2">Você ainda não possui reservas</p>
-          <Link href='/'>
-            <Button className="w-full mt-2">Fazer reserva</Button>
+    <main className="container mx-auto p-5">
+      <h1 className="text-xl font-semibold text-primaryDarker lg:text-2xl">
+        Minhas viagens
+      </h1>
+      {reservations.length > 0 ? (
+        <div className="flex flex-col lg:grid lg:grid-cols-3 lg:gap-12">
+          {reservations.map((reservation) => (
+            <UserReservationItem
+              key={reservation.id}
+              reservation={reservation}
+              fetchReservations={fetchReservations}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-1 flex-col items-center justify-center">
+          <p className="mt-2 text-center font-medium text-primaryDarker">
+            Você ainda não possui reservas
+          </p>
+          <Link href="/">
+            <Button className="mt-2 w-full lg:min-w-[20rem]">Fazer reserva</Button>
           </Link>
         </div>
       )}
     </main>
-  )
+  );
 }
